1d0723549fd59006789fc091fa39b0bcf82d405a
Finnr <finn.reinhardt@gmx.de>
2025-12-01 16:39:47 +0100

    add image_picker

---
+++ image_picker.js
@@ -0,0 +1,102 @@
+import { download_image } from "./util";
+
+
+export class ImageItem {
+    constructor(id, image) {
+        this.id = id;
+        this.image = image; 
+    }
+}
+
+
+export default class ImagePicker{
+    constructor(urls, queue_length, allready_inserted_cap) {
+        this.urls = urls;
+        this.queue_length = queue_length;
+        this.queue = new Array(this.queue_length).fill(0);
+        this.allreadyInsertedCap = allready_inserted_cap;
+        this.cache = {};
+    }
+
+    //[12232424]
+
+    allreadyInserted(id){
+        
+        let i = this.queue.length - 1;
+
+        
+        while(i >= this.queue.length - this.allreadyInsertedCap) {
+            if(i < 0) break;
+            currentItem = this.queue[i];
+            i--;
+            if(!(currentItem instanceof ImageItem)) continue;
+            if(currentItem.id === id) return true;
+        }
+
+        return false;
+
+    }
+
+    getRandomUrl(){
+
+        let random_index = Math.floor(Math.random() * this.urls.length)
+        let random_url = this.urls[random_index];
+
+        while(this.allreadyInserted(random_url)) {
+                random_index = Math.floor(Math.random() * this.urls.length)
+                random_url = this.urls[random_index];
+        }
+
+        return random_url;
+    }
+
+
+    async init() {
+
+        for (let index = 0; index < this.queue.length; index++) {
+            const random_url = this.getRandomUrl();
+            this.setImage(random_url, i);            
+        }
+
+    }
+
+    async setImage(url, i) {
+
+        let img;
+
+        if(this.cache[url]) {
+            img = this.cache[url];
+        } else {
+            img = await this.download_image(url);
+            this.cache[url] = img;
+        } 
+
+        if(i < 0) {
+            this.queue.push(
+                new ImageItem(
+                    url, 
+                    img
+                )
+            )
+        } else {
+            this.queue[index] = new ImageItem(
+                url,
+                img
+            );            
+        }
+
+    }
+
+
+    async nextImage(){
+
+        const random_url = this.getRandomUrl();
+        const new_image = this.queue.shift();
+        this.setImage(random_url, -1);
+
+        return new_image
+
+
+    }
+
+}
